5f5db2c1ca5faa92d5354a10e08a29a670949ff1
Hakan Mollaahmetoğlu <75783413+hakanmolla@users.noreply.github.com>
2023-06-19 13:28:01 +0300

    Update config.js

--- sosyal-app/src/firebase/config.js
+++ sosyal-app/src/firebase/config.js
@@ -4,12 +4,7 @@ import {getAuth} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDruf-GUULXFDr4BBGLnTKcvH4SekI9uS4",
-    authDomain: "aos-sosyal-app-7eb95.firebaseapp.com",
-    projectId: "aos-sosyal-app-7eb95",
-    storageBucket: "aos-sosyal-app-7eb95.appspot.com",
-    messagingSenderId: "155114784614",
-    appId: "1:155114784614:web:b94e7394835de64608eaa7"
+   
   };
 
   const app=initializeApp(firebaseConfig)
